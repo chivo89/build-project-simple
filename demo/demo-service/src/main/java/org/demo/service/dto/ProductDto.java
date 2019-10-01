15b72ca0e7a49749e4f851d19e93aafb1685428c
Chi Vo <chi.vo@persol.co.jp>
2019-10-01 10:51:22 +0700

    refs#buid sample code

--- demo/demo-service/src/main/java/org/demo/service/dto/ProductDto.java
+++ demo/demo-service/src/main/java/org/demo/service/dto/ProductDto.java
@@ -4,10 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 @Data
 @EqualsAndHashCode(callSuper = false)
 @AllArgsConstructor
+@NoArgsConstructor
 @Builder
 public class ProductDto {
 	private String id;
