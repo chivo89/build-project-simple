31dc0d4585be2c0a09a4a81b4349532ddf47c0a0
Chi Vo <chi.vo@persol.co.jp>
2019-10-02 12:58:12 +0700

    refs# add layout

--- demo/demo-web-app/src/main/resources/static/src/App.vue
+++ demo/demo-web-app/src/main/resources/static/src/App.vue
@@ -1,13 +1,32 @@
 <template>
   <div id="app">
+    <HeaderView></HeaderView>
     <img src="./assets/logo.png">
     <router-view/>
+    <FooterView></FooterView>
   </div>
 </template>
 
 <script>
+import HeaderView from './components/layouts/Header.vue';
+import FooterView from './components/layouts/Footer.vue';
+
 export default {
-  name: 'App'
+  name: 'App',
+  data() {
+		return {
+		};
+	},
+	created() {
+	},
+	updated() {
+	},
+	destroyed() {
+	},
+  components: {
+    HeaderView,
+    FooterView
+  }
 }
 </script>
 
